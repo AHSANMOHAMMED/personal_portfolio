40682f134d3667b5d1260bab506ecf19a33a63e1
support <ahsanmohammed828@gmail.com>
2026-09-07 23:40:06 +0530

    fix: import ScrollTrigger in app/page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from 'react'
 import Lenis from 'lenis'
-import { gsap } from '@/lib/gsap'
+import { gsap, ScrollTrigger } from '@/lib/gsap'
 import LoadingScreen from '@/components/sections/LoadingScreen'
 import Navbar from '@/components/ui/Navbar'
 import CustomCursor from '@/components/ui/CustomCursor'
